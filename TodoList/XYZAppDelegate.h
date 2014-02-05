3451def8b9c9e46b2259e635685545182cd15e9b
Nadeem Khan <nkhan@okta.com>
2014-02-04 22:56:45 -0800

    Initial Commit

---
+++ TodoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  TodoList
+//
+//  Created by Nadeem Khan on 2/4/14.
+//  Copyright (c) 2014 Nadeem Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by Nadeem Khan on 2/4/14.
+//  Copyright (c) 2014 Nadeem Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
